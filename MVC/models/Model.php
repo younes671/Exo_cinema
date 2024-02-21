8119db1c66d5e50fedaad05b43644da9511c20ba
Younesse AL AMRI <younes671@live.fr>
2024-02-21 08:38:21 +0100

    first commit

---
+++ MVC/models/Model.php
@@ -0,0 +1,16 @@
+<?php
+    const HOST_NAME = 'loclahost';
+    const DB_NAME = 'cinema';
+    const USER_NAME = 'root';
+    const PWD = '';
+
+    try
+    {
+        $connexion = 'mysql:host=' . HOST_NAME . ';dbname=' . DB_NAME;
+        $monPDO = new PDO($connexion,USER_NAME,PWD, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
+    }
+    catch(PDOException $e)
+    {
+        $msg = 'Erreur de connexion à la DB' . $e->getMessage();
+        die($message);
+    }
